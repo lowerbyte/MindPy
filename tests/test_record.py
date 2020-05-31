734844bee23e08316d69944f771430406f73761d
lowerbyte <fdziki6@gmail.com>
2020-05-31 11:28:37 +0000

    Adding tests to test_record.py

--- tests/test_record.py
+++ tests/test_record.py
@@ -1,11 +1,13 @@
 from core.record import Record, RecordOnScreen
 from tests.test_visitor import simple_tree
+import curses
+import pytest
 
 def test_add_child():
     parent = RecordOnScreen()
     parent.add_child(RecordOnScreen())
 
-    assert len(p.children) == 1
+    assert len(parent.children) == 1
 
 
 def test_remove_child():
@@ -21,3 +23,41 @@ def test_delete(simple_tree):
     simple_tree.children[0].delete()
 
     assert len(simple_tree.children) == 4
+
+def test_edit():
+    rec = RecordOnScreen()
+    rec.data = 'Test data'
+
+    rec.edit('New test data')
+
+    assert rec.data == 'New test data'
+
+@pytest.mark.parametrize('key', [
+    curses.KEY_RIGHT,
+    curses.KEY_DOWN,
+    curses.KEY_UP,
+    curses.KEY_LEFT,
+])
+def test_select(simple_tree, key):
+    test_rec = simple_tree.children[2]
+    selected = RecordOnScreen.select(test_rec, key)
+
+    if key == curses.KEY_RIGHT:
+        assert selected == test_rec.children[0]
+    elif key == curses.KEY_DOWN:
+        assert selected == test_rec.parent.children[1]
+    elif key == curses.KEY_UP:
+        assert selected == test_rec.parent.children[3]
+    elif key == curses.KEY_RIGHT:
+        assert selected == test_rec.parent
+
+def test_to_json():
+    rec = RecordOnScreen(0,0)
+    rec.children = [RecordOnScreen()]
+
+    json_dir = rec.toJSON()
+
+    assert json_dir == {"data": "None", "y": 0, "x": 0, "children": 
+                        [{"data": "None", "y": 0, "x": 0, "children": []}]}
+
+
